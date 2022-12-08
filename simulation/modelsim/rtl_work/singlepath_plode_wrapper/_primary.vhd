d04414e8e4efabacfa09a8ef61b581e051126b8f
hasanmutlu26 <hmutlu2626@gmail.com>
2022-12-08 12:29:11 +0300

    github fix

--- simulation/modelsim/rtl_work/singlepath_plode_wrapper/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity singlepath_plode_wrapper is
-    port(
-        pathResult      : out    vl_logic;
-        pathInput       : in     vl_logic
-    );
-end singlepath_plode_wrapper;
